b9c6352b0ac862d3978e11c48fb64d71f9ccd747
KKhimmoon <khin292546@gmail.com>
2023-10-05 22:40:55 +0700

    Assignment8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { PromoteCard } from '@/components/PromoteCard'
 import Banner from '@/components/Banner'
-import ProductCard from '@/components/ProductCard'
-import CardPanel from '@/components/CardPanel'
+
 export default function Home() {
   return (
     <main>
         <Banner/>
+        <PromoteCard></PromoteCard>
     </main>
   )
 }
